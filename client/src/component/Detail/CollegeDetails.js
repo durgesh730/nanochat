b018b791542b6a2cd67a088683a976e20d6a39da
durgeshUnico <durgesh.unicoglobal@gmail.com>
2023-10-29 15:45:54 +0530

    props add to some colleges

--- client/src/component/Detail/CollegeDetails.js
+++ client/src/component/Detail/CollegeDetails.js
@@ -3,29 +3,17 @@ import Navbar from "../Navbar";
 import Footer from "../Footer";
 import "../../style/aiimsdelhi.css";
 import SocialMedia from "../SocialMedia";
-import pic from '../../images/aiimsdelhi.jpg'
-import Infrastructure from '../../images/infrastructure.png'
-import hostel from '../../images/hostel.png'
-import located from '../../images/location.png'
 import Videos from "../Videos"
 import '../../style/video.css'
+import { useNavigate } from "react-router-dom";
 
-const CollegeDetails = ({ info }) => {
+const CollegeDetails = ({ info, Videosdata }) => {
 
-  const Videosdata = [
-    {
-      title: "AIIMS Rishikesh NEET Cut Off Marks & Rank 2021",
-      videoUrl: "https://www.youtube.com/embed/7CLzzaN0fis"
-    },
-    {
-      title: "AIIMS Rishikesh NEET Cut Off Marks & Rank 2021",
-      videoUrl: "https://www.youtube.com/embed/7CLzzaN0fis"
-    },
-    {
-      title: "AIIMS Rishikesh NEET Cut Off Marks & Rank 2021",
-      videoUrl: "https://www.youtube.com/embed/7CLzzaN0fis"
-    },
-  ]
+  const navigate = useNavigate()
+
+  const handleLocation = (location) => {
+    navigate(location)
+  }
 
   return (
     <>
@@ -35,12 +23,12 @@ const CollegeDetails = ({ info }) => {
         <div className="collgeAbout" >
 
           <div className="collegeHeading" >
-            <span>AIIMS Delhi</span>
+            <span>{info?.title}</span>
           </div>
 
           <div className="CollegeContainer" >
             <div className="CollegeImage" >
-              <img src={pic} alt="college image" />
+              <img src={info?.about?.collegeImgUrl} alt="college image" />
             </div>
 
             <div className="CollegeText" >
@@ -49,14 +37,15 @@ const CollegeDetails = ({ info }) => {
               </div>
               <div className="collegeAboutDetails" >
                 <span>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Suspendisse ultrices gravida dictum fusce. Non pulvinar neque laoreet suspendisse interdum consectetur libero id. Vitae ultricies leo integer malesuada nunc.
+                  {info?.about?.paraFirst}
                 </span> <br /><br />
 
                 <span>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Suspendisse ultrices gravida dictum fusce. Non pulvinar neque laoreet suspendisse interdum consectetur libero id. Vitae ultricies leo integer malesuada nunc. Pellentesque sit amet porttitor eget dolor morbi non.                 </span>
+                  {info?.about?.paraSecond}
+                </span>
               </div>
               <div className="learn-btn" >
-                <button>Learn More</button>
+                <button onClick={() => handleLocation(info?.about?.btnLink)} >Learn More</button>
               </div>
             </div>
           </div>
@@ -98,7 +87,7 @@ const CollegeDetails = ({ info }) => {
 
           <div className="quickPara" >
             <span>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Suspendisse ultrices gravida dictum fusce. Non pulvinar neque laoreet suspendisse interdum consectetur libero id. Vitae ultricies leo integer malesuada nunc.
+              {info?.quickSummary?.paraGraph}
             </span>
           </div>
 
@@ -113,13 +102,13 @@ const CollegeDetails = ({ info }) => {
               <div> Affiliated University  </div>
             </div>
             <div >
-              <div> 1920 </div>
-              <div> Govt. </div>
-              <div> 33</div>
-              <div> AIIMS, Ansari Nagar, New Delhi-110029, India </div>
-              <div> Click here </div>
-              <div> 91-11-26588663 / 26588641 </div>
-              <div> Self Funded</div>
+              <div>  {info?.quickSummary?.established} </div>
+              <div>  {info?.quickSummary?.institutionType}</div>
+              <div> {info?.quickSummary?.courses} </div>
+              <div>  {info?.quickSummary?.address}</div>
+              <div>  {info?.quickSummary?.officialSite}</div>
+              <div>  {info?.quickSummary?.contacts} </div>
+              <div>  {info?.quickSummary?.affiliatedUniversity}</div>
             </div>
           </div>
         </div>
@@ -131,7 +120,7 @@ const CollegeDetails = ({ info }) => {
 
           <div className="CollegeContainer" >
             <div className="CollegeImage" >
-              <img src={pic} alt="college image" />
+              <img src={info?.courses?.img} alt="college image" />
             </div>
 
             <div className="CollegeText" >
@@ -140,7 +129,9 @@ const CollegeDetails = ({ info }) => {
                   <span>Seats</span>
                 </div>
                 <div>
-                  <span>There is just enough space here for several lines of text. Make sure you get your message across clearly yet concisely.</span>
+                  <span>
+                    {info?.courses?.seats}
+                  </span>
                 </div>
               </div>
               <div>
@@ -148,7 +139,9 @@ const CollegeDetails = ({ info }) => {
                   <span>Tution Fee</span>
                 </div>
                 <div>
-                  <span>There is just enough space here for several lines of text. Make sure you get your message across clearly yet concisely.</span>
+                  <span>
+                    {info?.courses?.tutionfee}
+                  </span>
                 </div>
               </div>
               <div>
@@ -156,7 +149,9 @@ const CollegeDetails = ({ info }) => {
                   <span>Ranking</span>
                 </div>
                 <div>
-                  <span>There is just enough space here for several lines of text. Make sure you get your message across clearly yet concisely.</span>
+                  <span>
+                    {info?.courses?.ranking}
+                  </span>
                 </div>
               </div>
             </div>
@@ -172,48 +167,47 @@ const CollegeDetails = ({ info }) => {
           <div className="admissionDetails">
             <div className="titleDetails" >
               <p>
-                Find AIIMS New Delhi additional details given by this medical college
-                to Medical Counselling committee.
+                {info?.addmision?.para}
               </p>
             </div>
             <table className="tabletext">
               <tr>
                 <td>Male Hostel</td>
-                <td>yes</td>
+                <td> {info?.addmision?.maleHostel}</td>
               </tr>
               <tr>
                 <td>female Hostel</td>
-                <td>yes</td>
+                <td>{info?.addmision?.femaleHostel}</td>
               </tr>
               <tr>
                 <td>Other Info</td>
                 <td>
-                  Academic Fee and Hostel fee is one time fee for the entire course
+                  {info?.addmision?.otherInfo}
                 </td>
               </tr>
               <tr>
                 <td>Name of the Dean</td>
-                <td>DR NEENA KHANNA</td>
+                <td> {info?.addmision?.deanName}</td>
               </tr>
               <tr>
                 <td>Name of the Nodal Officer</td>
-                <td>DR ASHOK KUMAR JARYAL</td>
+                <td>{info?.addmision?.nodalName}</td>
               </tr>
               <tr>
                 <td>Designation</td>
-                <td>Dean</td>
+                <td>{info?.addmision?.designation}</td>
               </tr>
               <tr>
                 <td>Office Address</td>
-                <td>ACADEMIC SECTION AIIMS NEW DELHI 110029</td>
+                <td>{info?.addmision?.officeAddress}</td>
               </tr>
               <tr>
                 <td>Phone</td>
-                <td>011-26594365</td>
+                <td>{info?.addmision?.phone}</td>
               </tr>
               <tr>
                 <td>Email</td>
-                <td>durgeshchoudary@gmail.com</td>
+                <td>{info?.addmision?.email}</td>
               </tr>
             </table>
           </div>
@@ -224,24 +218,22 @@ const CollegeDetails = ({ info }) => {
             <span>Bond and Stipend</span>
           </div>
 
-          <div className="bondtext" ><p>
-            After MBBS course completion in All India Institute of Medical
-            Sciences Delhi, candidates should undergo rural service . If they
-            skip, they need to pay bond penalty.</p>
+          <div className="bondtext" >
+            <p>{info?.bondStipend?.para}</p>
           </div>
 
           <table className="bondtable">
             <tr>
               <td>Total Years</td>
-              <td>0</td>
+              <td>{info?.bondStipend?.totalYear}</td>
             </tr>
             <tr>
               <td>Penalty</td>
-              <td>₹ 0</td>
+              <td>₹ {info?.bondStipend?.penalty}</td>
             </tr>
             <tr>
               <td>Stipend</td>
-              <td>₹ 26300 /month</td>
+              <td>₹ {info?.bondStipend?.stipend}</td>
             </tr>
           </table>
         </div>
@@ -265,106 +257,92 @@ const CollegeDetails = ({ info }) => {
               <td>UG</td>
               <td>OneTime</td>
               <td>Tuition Fees</td>
-              <td>1350</td>
+              <td>{info?.fee?.tutionfee}</td>
             </tr>
             <tr>
               <td>UG</td>
               <td>OneTime</td>
               <td>Pot Fund Hostel</td>
-              <td>1320</td>
+              <td>{info?.fee?.potfundHostel}</td>
             </tr>
 
             <tr>
               <td>UG</td>
               <td>OneTime</td>
               <td>Hostel Rent For 5 Years</td>
-              <td>990</td>
+              <td>{info?.fee?.hostelRent}</td>
             </tr>
             <tr>
               <td>UG</td>
               <td>Monthly</td>
               <td>Mess Fees NonVeg</td>
-              <td>3500</td>
+              <td>{info?.fee?.messFeeNonVeg}</td>
             </tr>
             <tr>
               <td>UG</td>
               <td>OneTime</td>
               <td>Gymkhana Fee Hostel</td>
-              <td>220</td>
+              <td>{info?.fee?.gymfee}</td>
             </tr>
             <tr>
               <td>UG</td>
               <td>OneTime</td>
               <td>Hostel Deposit Refundable</td>
-              <td>1000</td>
+              <td>{info?.fee?.hosteldeposit}</td>
             </tr>
             <tr>
               <td>UG</td>
               <td>OneTime</td>
               <td>Electricity Charges</td>
-              <td>198</td>
+              <td>{info?.fee?.electricityCharge}</td>
             </tr>
             <tr>
               <td>UG</td>
               <td>OneTime</td>
               <td>Mess Deposit Refundable</td>
-              <td>500</td>
+              <td>{info?.fee?.messDeposit}</td>
             </tr>
             <tr>
               <td>UG</td>
               <td>OneTime</td>
               <td>Caution Deposit</td>
-              <td>100</td>
+              <td>{info?.fee?.cautionDeposit}</td>
             </tr>
             <tr>
               <td>UG</td>
               <td>OneTime</td>
               <td>Laboratory Fees</td>
-              <td>90</td>
+              <td>{info?.fee?.laboratoryfee}</td>
             </tr>
             <tr>
               <td>UG</td>
               <td>OneTime</td>
               <td>Registration Fee</td>
-              <td>20</td>
+              <td>{info?.fee?.registrationfee}</td>
             </tr>
             <tr>
               <td>UG</td>
               <td>OneTime</td>
               <td>Student Union Fee</td>
-              <td>53</td>
+              <td>{info?.fee?.studentUnionFee}</td>
             </tr>
             <tr>
               <td>UG</td>
               <td>Monthly</td>
               <td>Mess Fees Veg</td>
-              <td>3200</td>
+              <td>{info?.fee?.messFeeVeg}</td>
             </tr>
           </table>
         </div>
         <div className="Infrastructure">
           <div className="infrastructureTitle">
             <span>Infrastructure</span>
-            <img src={Infrastructure} alt="Infrastructure img" />
+            <img src={info?.infrastructure?.img} alt="Infrastructure img" />
           </div>
           <p>
-            &#x2022; AIIMS campus is divided into 5 locations, the East Campus
-            Includes OPD ward, main hospital ward, infirmary, medical school,
-            research department, convergence ward, BB Dixit library,
-            administration wing, student dormitories, central lawn, Jawaharlal
-            auditorium, and staff room. This eastern city also has 4 centers
-            specializing in cardiac carcinoma and neurology, an Oncology Center,
-            an Ophthalmic Science Center, and a Dental Education and Research
-            Center. <br></br>The library is open 24 hours a day, 7 days a
-            week (356 days) and has more than 1,50,257 books and other documents
-            such as bound newspapers, pamphlets, etc.and employees, technicians
-            and non-technicians. An average of 320 readers visit the library every
-            day.<br></br> &#x2022; The library is well equipped with modern
-            facilities to support biomedical teaching and research.;A library has
-            predominantly electronic resources (80%) that are provided with
-            anytime, anywhere access to physicians and faculty members for their
-            academic activities. Access Medicine Books, Oxford Medicine Online (53
-            textbooks), OVID
+            &#x2022;
+            {info?.infrastructure?.paraFirst}<br></br>
+            &#x2022; {info?.infrastructure?.paraSecond}
           </p>
         </div>
 
@@ -374,42 +352,22 @@ const CollegeDetails = ({ info }) => {
         <div className="hostel">
           <p>
             {" "}
-            &#x2022; AIIMS has more than one accommodation section with all sorts of
-            facilities. Twelve male and 26 female office attendants are posted in
-            different hostels. AIIMS has several halls of residence with a
-            capacity of over 1990 accommodation facilities with single/double
-            rooms, 418 accommodations for nursing students and staff nurses and 24
-            guest rooms furnished with all facilities. <br /><br />
-            &#x2022; These are spread over different hostels and residences in the AIIMS Main Campus, Masjid
-            Moth, Ayurvigyan Nagar, and JPNA Trauma Centre Hostel available in Raj
-            Nagar. Male hostels have a total of more than 1350 student
-            accommodation facilities. Female hostel has 650 accommodation
-            facilities, Staff Hostel 400 accommodation, and also Guest rooms are
-            available here. All residences have common rooms, sports clubs,
-            Gymnasium, reading rooms, and recreational facilities.
+            &#x2022;{info?.hostel?.paraFirst} <br /><br />
+            &#x2022; {info?.hostel?.paraSecond}
           </p>
           <div className="hosteltitle">
-            <img src={hostel} alt="hostel img" />
+            <img src={info?.hostel?.img} alt="hostel img" />
           </div>
         </div>
 
         <div className="Infrastructure">
           <div className="infrastructureTitle">
             <span> Affilliated Hospitals</span>
-            <img src={Infrastructure} alt="Infrastructure img" />
+            <img src={info?.hospital?.img} alt="Infrastructure img" />
           </div>
           <p>
-            &#x2022;AIIMS is currently not a single hospital, but a conglomerate of
-            facilities. It has over 1,500 units located in the main hospital, the
-            Ophthalmological Center of dr.Rajendra Prasad, Cardiac Surgery Center,
-            Neuroscience Center, Hospital Center, Rotary Institute, and Detox
-            Center. This facility complex serves approximately 1.5 million
-            outpatients and 80,000 inpatients annually. <br /> <br />
-            &#x2022; The number of surgical  operations performed at AIIMS last year amounted to over 100,000. The
-            total number of beds available at Major Hospital and Dr.B.R. Ambedkar
-            Institute Rotary Cancer Hospital, Dr.RP Center, NDDTC, CCM, NCI
-            Jhajjar has 2,792 beds, including 288 private sector beds. Rural/Urban
-            Health Training Centres Ballabgarh 35 km distance from the college
+            &#x2022;{info?.hospital?.paraFirst} <br /> <br />
+            &#x2022; {info?.hospital?.paraSecond}
           </p>
         </div>
 
@@ -418,12 +376,7 @@ const CollegeDetails = ({ info }) => {
             <span className='font-weight-bold'>Counselling Information</span>
           </div>
           <div className="CounsellingPara" >
-            <span>All the NEET Under Graduate (MBBS/BDS) aspirant candidates are hereby informed that counseling for All India Quota seats/
-              Institutional Quota/ Domicile (internal candidates) of Central Universities (Aligarh Muslim University/ Banaras Hindu University/
-              University of Delhi/Faculty of Dentistry, Jamia Millia Islamia, Delhi), (wards of ESIC insured persons)
-              seats of colleges under Employee State Insurance Corporation and Armed Forces Medical College, Pune for the Under graduate session 2023-24 will
-              be conducted by Medical Counseling Committee/ Directorate General of Health Services, Ministry of Health and Family Welfare, Government of
-              India. <a href="#">click here for more</a></span>
+            <span>{info?.counselling?.para} <a href={info?.counselling?.link}>click here for more</a></span>
           </div>
           <div className='yotubeVideo' >
             {Videosdata.map((item, index) => {
@@ -438,18 +391,8 @@ const CollegeDetails = ({ info }) => {
           <span>How to Reach</span>
         </div>
         <div className="hostel">
-          <p>
-            New Delhi Bus Stand and Inter State Bus Terminal to AIIMS Medical
-            college is 20 km distance. We can reach within 40 min from NH 44 and
-            Baba Banda Singh Bahadur Setu road. Ajmeri Gate Railway station in
-            Delhi is located 10.5 km away from this college, we can reach here
-            within 30 min through Prithviraj Road and Sri Aurobindo Marg. Indira
-            Gandhi International Airport, Delhi is the nearest airport of this
-            medical college, which is located 9.8 km away from Medical College.
-            Through Mahatma Gandhi Road and NH 48, we can reach the Airport within
-            20 min to this Medical College.
-          </p>
-          <img src={located} alt="hostel img" />
+          <p>{info?.location?.para}</p>
+          <img src={info?.location?.img} alt="hostel img" />
         </div>
       </div>
       <Footer />
