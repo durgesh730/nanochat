9773c3078cc88a7e2d83d4d5daa43693a0ec2de2
durgeshUnico <durgesh.unicoglobal@gmail.com>
2023-12-10 00:34:54 +0530

    server re deploy

--- client/src/host.js
+++ client/src/host.js
@@ -1,4 +1,4 @@
-export const serverhost = "http://localhost:8009"
-// export const serverhost = "https://nanchatserver.vercel.app"
+// export const serverhost = "http://localhost:8009"
+export const serverhost = "https://nanochatserver.vercel.app"
 // export const serverhost = "https://nanochatser.onrender.com"
 
