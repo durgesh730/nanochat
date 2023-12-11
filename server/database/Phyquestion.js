7a7eb7083c08f0a7c4c7e7bda038f6f902d4db01
Muskanbatra <mbatra1500@gmail.com>
2023-12-11 11:45:01 +0530

    questions update

--- server/database/Phyquestion.js
+++ server/database/Phyquestion.js
@@ -1,71 +1,116 @@
 export default [
     {
         id: 1,
-        topic:"Physics",
-        question : "A light year is a unit of",
-        options : [
-            'Time',
-            'Mass',
-            'Distance',
-            'Energy',
+        topic: "Physics",
+        question: "Pick the wrong answer is the context with rainbow",
+        options: [
+            'An observer can see a rainbow when his front is towards the sun ',
+            'Rainbow is a combined effect of dispersion, refraction and reflection of sunlight.',
+            'When the light rays undergo two internal reflections in a water drop, a secondary rainbow is formed.',
+            ' The order of colours is reversed in the secondary rainbow.',
         ]
     },
     {
         id: 2,
-        topic:"Physics",
-        question : "Which of the following is not equal to watt?",
-        options : [
-            'Joule/second',
-            'Ampere*volt',
-            '(Ampere*Ampere)*ohm',
-            'Ampere/volt',
+        topic: "Physics",
+        question: "A mass m is attached to a thin wire and whirled in a vertical circle. The wire is most likely to break when:",
+        options: [
+            'the mass is at the lowest point',
+            'inclined at an angle of 60º from vertical',
+            'the mass is at the highest point',
+            'the wire is horizontal',
         ]
     },
     {
         id: 3,
-        topic:"Physics",
-        question : "Newton-second is unit of",
-        options : [
-            'Veclocity',
-            'Angular momentum',
-            'Momentum',
-            'Energy',
+        topic: "Physics",
+        question: ". Two point charges A and B, having charges +Q and -Q respectively, are placed at certain distance apart and force acting between them is F. If 25% charge of A is transferred to B, then force between the charges becomes:",
+        options: [
+            '16F/9',
+            '4F/3',
+            'F',
+            '9F/16',
         ]
     },
     {
         id: 4,
-        topic:"Physics",
-        question : "What does the toString() method return?",
-        options : [
-            'Return Object',
-            'Return String',
-            'Return Integer',
-            'Return Integer'
+        topic: "Physics",
+        question: " When a block of mass M is suspended by a long wire of length L, the length of the wire becomes (L+l). The elastic potential energy stored in the extended wire is :",
+        options: [
+            '1/2 Mgl ',
+            '1/2 Mgl',
+            'Mgl',
+            'MgL'
         ]
     },
     {
         id: 5,
-        topic:"chemistry",
-        question : "Which function is used to serialize an object into a JSON string?",
-        options : [
-            'stringify()',
-            'parse()',
-            'convert()',
-            'convert()',
+        topic: "physics",
+        question: "A disc of radius 2m. and mass 100 kg rolls on a horizontal floor. Its centre of mass has speed of 20cm/s. How much work is needed to stop it ?",
+        options: [
+            '2J',
+            '1J',
+            '3J ',
+            '30KJ',
         ]
     },
     {
         id: 6,
-        topic:"physics",
-        question : "What does the toString() method return?",
-        options : [
-            'Return Object',
-            'Return String',
-            'Return Integer',
-               'Return Integer',
+        topic: "physics",
+        question: "In total internal reflection when the angle of incidence is equal to the critical angle for the pair of media in contact, what will be angle of refraction ?",
+        options: [
+            'equal to angle of incidence',
+            '90° ',
+            '180°',
+            '0°',
+        ]
+    },
+    {
+        id: 7,
+        topic: "Physics",
+        question: "Which colour of the light has the longest wavelength?",
+        options: [
+            'green',
+            'violet',
+            'red ',
+            'blue',
+        ]
+    },
+    {
+        id: 8,
+        topic: "Physics",
+        question: "The total energy of an electron in an atom in an orbit is -3.4eV. Its kinetic and potential energies are, respectively:",
+        options: [
+            '3.4eV, -6.8eV',
+            '3.4eV, 3.4eV',
+            '-3.4eV, -3.4eV',
+            '-3.4eV, -6.8eV ',
+        ]
+    },
+    {
+        id: 9,
+        topic: "Physics",
+        question: ". Increase in temperature of a gas filled in a container would lead to :",
+        options: [
+            'decrease in its pressure',
+            'decrease in intermolecular distance',
+            'increase in its mass',
+            'increase in its kinetic energy',
+        ]
+    },
+    {
+        id: 10,
+        topic: "physics",
+        question: "For a p-type semiconductor, which of the following statements is true ",
+        options: [
+            'Holes are the majority carriers and pentavalent atoms are the dopants.',
+            'Electrons are the majority carriers and pentavalent atoms are the dopants',
+            'Electrons are the majority carriers and trivalent atoms are the dopants.',
+            'Holes are the majority carriers and trivalent atoms are the dopants. ',
         ]
     },
 
+
 ]
 
-export const Phyanswers = [0,1,2,1,0,1];
+export const Phyanswers = [0,0,3,0,2,1,2,3,3,3];
--- server/database/data.js
+++ server/database/data.js
@@ -1,71 +1,116 @@
 export default [
     {
         id: 1,
-        topic:"chemistry",
-        question : "Javascript is an _______ language",
-        options : [
-            'Object-Oriented',
-            'Object-Based',
-            'Procedural',
-            'Procedural',
+        topic: "chemistry",
+        question: " In which case change in entropy is negative?",
+        options: [
+            'Sublimation of solid to gas',
+            '2H(g) → H2 (g)',
+            'Evaporation of water',
+            'Expansion of a gas at constant temperature',
         ]
     },
     {
         id: 2,
-        topic:"chemistry",
-        question : "Following methods can be used to display data in some form using Javascript",
-        options : [
-            'document.write()',
-            'console.log()',
-            'window.alert()',
-            'window.alert()',
+        topic: "chemistry",
+        question: "Under isothermal condition a gas at 300 K expands from 0.1 L to 0.25 L against a constant external pressure of 2 bar.The work done by the gas is :[Given that 1 L bar = 100J]",
+        options: [
+            '25 J',
+            '30 J',
+            '-30 J',
+            '5KJ',
         ]
     },
     {
         id: 3,
-        topic:"chemistry",
-        question : "When an operator value is NULL, the typeof returned by the unary operator is:",
-        options : [
-            'Boolean',
-            'Undefined',
-            'Object',
-            'Object',
+        topic: "chemistry",
+        question: " Enzymes that utilize ATP in phosphate transfer require an alkaline earth metal (M) as the cofactor M is",
+        options: [
+            'Ca',
+            'Sr',
+            'Be',
+            'Mg',
         ]
     },
     {
         id: 4,
-        topic:"chemistry",
-        question : "What does the toString() method return?",
-        options : [
-            'Return Object',
-            'Return String',
-            'Return Integer',
-            'Return Integer'
+        topic: "chemistry",
+        question: "For the second period elements the correct increasing order of first ionisation enthalpy is:",
+        options: [
+            'Li < B < Be',
+            'Li < Be< B < C < O < N < F < Ne',
+            'Li < Be < B < C < N < O < F < Ne',
+            'Li < B < Be < C < O < N < F < Ne'
         ]
     },
     {
         id: 5,
-        topic:"chemistry",
-        question : "Which function is used to serialize an object into a JSON string?",
-        options : [
-            'stringify()',
-            'parse()',
-            'convert()',
-            'convert()',
+        topic: "chemistry",
+        question: "Which of the following series of transitions in the spectrum of hydrogen atom falls in visible region?",
+        options: [
+            'Paschen series',
+            'Brackett series',
+            'Lyman series',
+            'Balmer series ',
         ]
     },
     {
         id: 6,
-        topic:"physics",
-        question : "What does the toString() method return?",
-        options : [
-            'Return Object',
-            'Return String',
-            'Return Integer',
-               'Return Integer',
+        topic: "chemistry",
+        question: "The non-essential amino acid among the following is:",
+        options: [
+            'alanine',
+            'lysine',
+            'valine',
+            'leucine',
         ]
     },
+    {
+        id: 7,
+        topic: "chemistry",
+        question: "Among the following, the one that is not a green house gas is :",
+        options: [
+            'ozone',
+            'sulphur dioxide',
+            'nitrous oxide',
+            'methane',
+        ]
+    },
+    {
+        id: 8,
+        topic: "chemistry",
+        question: "The mixture that forms maximum, boiling azeotrope is :",
+        options: [
+            'Acetone + Carbon disulphide',
+            'Heptane + Octane',
+            'Water + Nitric acid',
+            'Ethanol + Water',
+        ]
+    },
+    {
+        id: 9,
+        topic: "chemistry",
+        question: "4d, 5p,5f and 6p orbitals are arranged in the order of decreasing energy. The correct option is:",
+        options: [
+            '6p > 5f > 4d > 5p',
+            '5f > 6p > 4d > 5p',
+            '5f > 6p > 5p > 4d',
+            '6p > 5f > 5p > 4d',
+        ]
+    },
+    {
+        id: 10,
+        topic: "chemistry",
+        question: "Among the following the narrow spectrum antibiotic is :",
+        options: [
+            'amoxycillin',
+            'chloramphenicol',
+            'penicillin G ',
+            'ampicillin',
+        ]
+    },
+    
 
 ]
 
-export const answers = [0,1,2,1,0,1];
+export const answers = [1, 2, 3, 3, 3, 3, 0, 1, 2, 2, 2, 3, 0, 2, 0, 0, 2, 1, 2, 3, 1];
