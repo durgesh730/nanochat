437360551b99804ff3509245cd14032e441364ab
durgesh730 <durgesh.unicoglobal@gmail.com>
2024-03-25 11:24:37 +0530

    add admin

--- server/controller/user.js
+++ server/controller/user.js
@@ -47,7 +47,6 @@ export const UserRegister = async (req, res) => {
 
 export const UserLogin = async (req, res) => {
     const { email, password } = req.body;
-    console.log(req.body)
     if (!email || !password) {
         return res.status(400).json({ error: "Please fill all details." });
     }
