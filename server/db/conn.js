52b10e7f8a86845d3930bad4f2cb29b6cf39f5df
durgesh730 <durgesh.unicoglobal@gmail.com>
2024-03-30 23:12:51 +0530

    db string updated

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
-// import * as dotenv from 'dotenv' 
-// dotenv.config()
+import { config } from "dotenv";
+config('dotenv')
 
 mongoose.set('strictQuery', true);
-export default async function connect(){
-    mongoose.connect("mongodb+srv://Durgesh:Durgesh02042001@cluster0.skc6gmq.mongodb.net/?retryWrites=true&w=majority")
-    console.log("Database Connected")
+
+export default async function connect() {
+    mongoose.connect(process.env.DB_STRING)
+        .then((res) => {
+            console.log("Data Base connected")
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 }
